31b1056a53eade6978dcaa56d3ff4b1ae42bcd33
llziraell <98565914+llziraell@users.noreply.github.com>
2024-01-16 14:30:09 +0300

    project is ready for work

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import {BootstrapVueNext} from 'bootstrap-vue-next'
+
+import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.use(BootstrapVueNext)
 app.use(createPinia())
 app.use(router)
 
